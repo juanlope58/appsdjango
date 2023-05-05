c02f13a4702e90aeedf22ae09c87623dffefe232
juanlope58 <juan.loperaa@udea.edu.co>
2023-05-04 20:33:01 -0500

    view list author complete and author detail incomp

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -41,6 +41,6 @@ class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 2
 
-class AuthorDetailView(generic.DeleteView):
+class AuthorDetailView(generic.DetailView):
     model=Author
 
